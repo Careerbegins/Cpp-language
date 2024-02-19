95ee50599969ac5c3236157a923db4b05ef51ce7
Careerbegins <114606615+Careerbegins@users.noreply.github.com>
2024-02-19 18:34:30 +0530

    print.cpp

--- print.cpp
+++ print.cpp
@@ -5,11 +5,18 @@ int print(int arr[],int n){
     for (int i = 0; i < n; i++)
     {
         cout<<arr[i] <<" ";
-        i++;
+    }
+}
+int printReverse(int arr[],int n){
+    for (int i = n-1; i >= 0; i--)
+    {
+        cout<<arr[i] <<" ";
     }
 }
 int main(){
     int A[] = {1,2,3,4,5};
     int N = 5;
     print(A,N);
+    cout<<endl;
+    printReverse(A,N);
 }
