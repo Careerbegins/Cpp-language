f29379ac086db794da543610b2980c985dfa3ac8
Careerbegins <114606615+Careerbegins@users.noreply.github.com>
2024-02-19 17:22:58 +0530

    Update countNoOfElements.cpp

--- countNoOfElements.cpp
+++ countNoOfElements.cpp
@@ -1,20 +1,24 @@
 #include<iostream>
 using namespace std;
 
-int count(int A[6], int x){
-    for (int i = 0; i < 6; i++)
+int count(int A[], int x, int n){
+    int ans=0;
+    for (int i = 0; i < n; i++)
     {
-        if (A[i]>=x)
+        if (A[i]==x)
         {
-            cout<<i-1;
+            ans +=1;
         }
         
     }
+    cout<<ans;
+    return ans;
     
 }
 
 int main(){
-    int A[] = {1, 2, 4, 5, 8, 10};
-    int X = 9;
-    count(A, X);
+    int A[] = {1, 2, 4, 5, 4, 8,4,4,4, 10};
+    int X = 4;
+    int n = sizeof(A)/sizeof(A[0]);
+    count(A, X, n);
 }
